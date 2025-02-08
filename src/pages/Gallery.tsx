4e3bc200a49113adf894c6e9f25e5e5a46c3c3dd
SugarDiDi <denis.denisenko@gmail.com>
2025-02-08 13:27:00 +0200

    change to menu

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -31,7 +31,7 @@ const Gallery = () => {
           viewport={{ once: true }}
           className="text-4xl font-bold text-center mb-12 text-gradient"
         >
-          הגלריה שלנו
+          התפריט שלנו
         </motion.h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {images.map((image, index) => (
