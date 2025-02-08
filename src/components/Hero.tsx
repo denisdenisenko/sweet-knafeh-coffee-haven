2d484d16955f9631ef360a1516c0d2e7497ee02f
SugarDiDi <denis.denisenko@gmail.com>
2025-02-08 13:53:12 +0200

    removed button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -63,14 +63,6 @@ const Hero = () => {
           >
             לתפריט שלנו
           </Button>
-          <Button
-            size="lg"
-            variant="outline"
-            className="border-white text-white hover:bg-white/10 dark:border-white dark:text-white dark:hover:bg-white/10 transition-all duration-300 transform hover:scale-105"
-          >
-            <Coffee className="w-5 h-5 mr-2" />
-            להזמין עכשיו
-          </Button>
         </motion.div>
       </div>
 
